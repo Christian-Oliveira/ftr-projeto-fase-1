a77f86deb014242b0611f4bea3b8af66167e6000
Christian Oliveira <christianoliveirati@gmail.com>
2025-06-20 11:03:21 -0300

    feat: add schemas e migrations do bd

--- server/src/infra/db/schemas/urls.ts
+++ server/src/infra/db/schemas/urls.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'node:crypto'
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const urls = pgTable('urls', {
   id: text('id')
@@ -7,5 +7,6 @@ export const urls = pgTable('urls', {
     .$defaultFn(() => randomUUID()),
   originalUrl: text('original_url').notNull(),
   shortenedUrl: text('shortened_url').notNull().unique(),
+  accessCount: integer('access_count').default(0).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
 })
