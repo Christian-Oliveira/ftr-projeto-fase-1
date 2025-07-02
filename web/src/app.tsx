f35ce343d8798a4ea5774e294e79da0e83b39158
Christian Oliveira <christianoliveirati@gmail.com>
2025-07-02 20:29:27 -0300

    feat: ajustando componente de registrar url

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,15 @@
+import { ListUrls } from "./components/list-urls"
+import { RegisterUrlForm } from "./components/register-url-form"
+
 export function App() {
   return (
-    <h1>Hello World!!!</h1>
+    <main className="h-dvh flex flex-col gap-4 p-3 items-center md:flex-row md:justify-center md:items-center">
+      <span className="justify-items-center md:justify-items-start">
+        <img src="/logo.svg" alt="Logo" className="w-[97px] h-[24px]" />
+      </span>
+      <RegisterUrlForm />
+      <ListUrls />
+    </main>
   )
 }
 
---
+++ web/src/components/list-urls.tsx
@@ -0,0 +1,10 @@
+
+export function ListUrls() {
+  return (
+    <div className="bg-zinc-100 w-full max-w-[580px] h-[234px] rounded-sm">
+      <div className="p-[32px]">
+        <span className="text-lg font-bold">Meus links</span>
+      </div>
+    </div>
+  )
+}
